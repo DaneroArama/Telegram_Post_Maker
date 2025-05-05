06c6cf33eaab58e8eaddeccdf54c55ef220039b5
DaneroArama <www.aviciiguetta@gmail.com>
2025-05-05 09:50:58 +0630

    Style Change

--- src/utils/jobUtils.js
+++ src/utils/jobUtils.js
@@ -22,6 +22,7 @@ export const categories = [
   'Planning & Operations',
   'Project & Product Management',
   'Quality Control & Assurance',
+  'Research, Teaching & Training'
   'Translation'
 ]
 
